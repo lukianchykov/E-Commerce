dece39da1e55b87f0785e02ca12b3aee0c0c15db
Andrii Lukianchykov <a.lukianchykov@voxloud.com>
2023-04-01 15:42:56 +0300

    Minor change

--- src/main/resources/db/db.sql
+++ src/main/resources/db/db.sql
@@ -2,4 +2,3 @@ create database ecommercedb;
 create user 'ecommerceuser' identified by 'ecommercepassword';
 GRANT ALL PRIVILEGES ON ecommercedb.* TO 'ecommerceuser'@'%';
 FLUSH PRIVILEGES;
-# DROP USER 'ecommerceuser'@'%';
